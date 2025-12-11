49d81fa8a62bc0e22681a81fb3cae7a442d72192
Arnoldas Seputis <arnoldas.seputis@esrlabs.com>
2025-12-11 10:33:01 +0100

    Apply linter comments to AoC 2025 day2 part 2
    Just cleaning up the Python script to make the linter happy.

--- advent_of_code/2025/day2/run_part2.py
+++ advent_of_code/2025/day2/run_part2.py
@@ -1,13 +1,20 @@
+"""Solves the second part of the AoC 2025 day 2 problem."""
+
 import time
 from compare_substrings import compare_substrings
 
-INPUT_FILE ='input.txt'
+INPUT_FILE = 'input.txt'
 
 # PART 2
 
 f = open(INPUT_FILE)
-#invalid_numbers = [] #FOR DEBUGGING
+
+
 def main():
+    """
+    Loops over ranges like 11-50, 1200-1255, and so on and selects the ones
+    that have repeating patterns, lke 11, 44, 1212 and so on.
+    """
     result_sum = 0
     for range_str in f.read().split(','):
         start, end = [int(number) for number in range_str.split('-')]
@@ -18,15 +25,16 @@ def main():
             for n in range(2, i_len + 1):
                 if i_len % n == 0:
                     if compare_substrings(i_len, i_str, n):
-                        # invalid_numbers.append(i) #FOR DEBUGGING
                         result_sum += i
                         break
+    print(result_sum)
 
-    #print(invalid_numbers) #FOR DEBUGGING
-    print(result_sum) # 36037497037
 
 if __name__ == "__main__":
-    start = time.perf_counter()
+    start_time = time.perf_counter()
     main()
-    end = time.perf_counter()
-    print(f"Runtime: {end - start:.3f} seconds") # the median after 9 runs: 5.350 s
+    end_time = time.perf_counter()
+    print(f"Runtime: {end_time - start_time:.3f} seconds")
+
+# Answer: 36037497037
+# the median after 9 runs: 5.350 s
